a2bdcd510d77c9489a25741e75c30b3c8880dfeb
Глеб Галочкин <leonidgalockin@gmail.com>
2024-09-24 00:01:10 +0300

    HW_4

---
+++ HW_4/HW_4.js
@@ -0,0 +1,15 @@
+//Task 1
+
+
+const i = 0;
+while(i < 10){
+    i++;
+    if(i % 2 ==0)
+        console.log(i);
+}
+
+let num = 0
+while(num < 100) {
+    num++;
+    if(num % 2 == 0) console.log(num)
+}
